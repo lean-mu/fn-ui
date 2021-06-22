52dab43cd8757075b9321d57a2de9510004f28ab
olivier <orefalo@gmail.com>
2021-06-22 19:48:59 +0400

    force prefix

--- src/server/router.js
+++ src/server/router.js
@@ -1,14 +1,10 @@
 var express = require("express")
 var router = express.Router()
 
-const baseurl = "/ui"
-
-console.log("baseurl=" + baseurl)
-
-router.get(baseurl, require("./controllers/index.js"))
-router.use(baseurl + "/api/apps", require("./controllers/apps.js"))
-router.use(baseurl + "/api/fns", require("./controllers/functions.js"))
-router.use(baseurl + "/api/info", require("./controllers/info.js"))
-router.use(baseurl + "/api/stats", require("./controllers/stats.js"))
+router.get("/ui", require("./controllers/index.js"))
+router.use("/ui" + "/api/apps", require("./controllers/apps.js"))
+router.use("/ui" + "/api/fns", require("./controllers/functions.js"))
+router.use("/ui" + "/api/info", require("./controllers/info.js"))
+router.use("/ui" + "/api/stats", require("./controllers/stats.js"))
 
 module.exports = router
